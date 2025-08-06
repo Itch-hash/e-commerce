764c1d0fdefc911f3ba15c685b9145acd3811645
Itch-hash <contact.itch8@gmail.com>
2025-08-06 16:43:36 +0300

    edited image links in db to working ones // added all products to listing page dynamically

--- script.js
+++ script.js
@@ -26,4 +26,52 @@ document.addEventListener("DOMContentLoaded", () => {
       cartSidebar.classList.remove("active");
     }
   }
+
+  //Handling Products
+
+  fetch("./db.json")
+    .then((reponse) => {
+      return reponse.json();
+    })
+    .then((data) => {
+      const products = data.products;
+      const productList = document.getElementById("product-list");
+      products.forEach((product) => {
+        const productCardDiv = document.createElement("div");
+        const productImg = document.createElement("img");
+        const productInfoDiv = document.createElement("div");
+        const productTitle = document.createElement("h3");
+        const productDescription = document.createElement("p");
+        const productPrice = document.createElement("div");
+        const productActions = document.createElement("div");
+        const addToCartBtn = document.createElement("button");
+        const addToWishListBtn = document.createElement("button");
+
+        productCardDiv.classList.add("product-card");
+        productList.appendChild(productCardDiv);
+        productImg.classList.add("product-image");
+        productImg.src = product.thumbnail;
+        productImg.alt = product.title;
+        productCardDiv.appendChild(productImg);
+        productCardDiv.appendChild(productInfoDiv);
+        productInfoDiv.classList.add("product-info");
+        productInfoDiv.appendChild(productTitle);
+        productTitle.innerHTML = product.title;
+        productTitle.classList.add("product-title");
+        productInfoDiv.appendChild(productDescription);
+        productDescription.innerHTML = product.description;
+        productDescription.classList.add("product-description");
+        productInfoDiv.appendChild(productPrice);
+        productPrice.innerHTML = `$${product.price.toFixed(2)}`;
+        productPrice.classList.add("product-price");
+        productInfoDiv.appendChild(productActions);
+        productActions.classList.add("product-actions");
+        productActions.appendChild(addToCartBtn);
+        addToCartBtn.classList.add("add-to-cart-btn");
+        addToCartBtn.innerHTML = "Add to Cart";
+        productActions.appendChild(addToWishListBtn);
+        addToWishListBtn.classList.add("add-to-wishlist-btn");
+        addToWishListBtn.innerHTML = "❤️";
+      });
+    });
 });
